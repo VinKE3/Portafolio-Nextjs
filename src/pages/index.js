dbae9b10b4f9660e4b3c5a989a298eb5adecd4af
kevin munoz <harquin09@gmail.com>
2023-04-08 08:59:22 -0500

    about

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import profilePic from "../../public/images/profile/developer-pic-1.png";
 import AnimatedText from "@/components/AnimatedText";
 import Link from "next/link";
 import { LinkArrow } from "@/components/Icons";
+import HireMe from "@/components/HireMe";
+import lightBulb from "../../public/images/svgs/miscellaneous_icons_1.svg";
 
 export default function Home() {
   return (
@@ -29,24 +31,23 @@ export default function Home() {
                 className="!text-6xl !text-left"
               />
               <p className="my-4 text-base font-medium">
-                Como desarrollador full-stack capacitado, me dedico a convertir
-                ideas en aplicaciones web innovadoras. Explora mis últimos
-                proyectos y artículos, donde muestro mi experiencia en React.js
-                y desarrollo web.
+                Como desarrollador full-stack, me dedico a convertir ideas en
+                aplicaciones web innovadoras. Explora mis últimos proyectos y
+                artículos, donde muestro mi experiencia en React.js y desarrollo
+                web.
               </p>
               <div className="flex items-center self-start mt-2">
                 <Link
                   href="/dummy.pdf"
                   target={"_blank"}
-                  className="
-                 flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold
+                  className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold
                   hover:bg-light hover:text-dark border-2 border-solid border-transparent 
                   hover:border-dark transition duration-300 ease-in-out
                 "
                   download={true}
                 >
                   Resumen PDF
-                  <LinkArrow className={"w-6 ml-1"} />
+                  <LinkArrow className={"!w-5 ml-1"} />
                 </Link>
                 <Link
                   href="kevin.munoz0995@gmail.com"
@@ -61,6 +62,10 @@ export default function Home() {
             </div>
           </div>
         </Layout>
+        <HireMe />
+        <div className="absolute right-8 bottom-8 inline-block w-24">
+          <Image src={lightBulb} alt="vinke" className="w-full h-auto" />
+        </div>
       </main>
     </>
   );
